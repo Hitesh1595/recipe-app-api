a87bfd452fb80674e7890d2fc0ec4f3d0a0c1871
Hitesh1595 <hiteshpratap82@gmail.com>
2024-11-19 23:05:43 +0530

    Docer Command and Wait for DB Setup

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,9 +2,9 @@
 Sample test
 """
 
-
 from django.test import SimpleTestCase
 from app import calc
+
 
 class ClacTests(SimpleTestCase):
     """ Test the cacl module """
@@ -13,7 +13,7 @@ class ClacTests(SimpleTestCase):
         """ Test adding number together """
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_number(self):
         """ Test Subtract the number """
         res = calc.subtract(3, 5)
